a6e99fcafbd3aff5b2a1944dfcd56e186afc8055
Jason D. McCormick <jason@mfamily.org>
2025-06-08 15:36:05 -0400

    prep release

--- asl3-init.sh
+++ asl3-init.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+
+# install the repo
 apt update
 apt upgrade -y
 apt install -y wget
 wget https://repo.allstarlink.org/public/asl-apt-repos.deb12_all.deb
 dpkg -i asl-apt-repos.deb12_all.deb
 apt update
-apt install -y asl3-asterisk dahdi-dkms
 
+# install kernel headers in preparation
+apt install -y linux-headers-$(uname -r)
 
+# prep /etc/asterisk
+if [ ! -f /etc/asterisk/asterisk.conf ]; then
+	rm -rf /etc/asterisk/*
+fi
+chown asterisk:asterisk /etc/asterisk
+
+# install
+apt install -y asl3 
--- asl3/asl3.sh
+++ asl3/asl3.sh
@@ -7,5 +7,17 @@ modprobe dahdi
 modprobe dahdi_transcode
 modprobe dahdi_dummy
 
+# Prepare missing configuration (if needed)
+chown asterisk:asterisk /etc/asterisk
+if [ ! -f /etc/asterisk/asterisk.conf ]; then
+	apt install --reinstall -o Dpkg::Options::="--force-confmiss" asl3-asterisk-config
+fi
+
+# Fix defaults that don't work in a container
+perl -pi -e 's/node_lookup_method\s+=\s+both/node_lookup_method = dns/g' \
+	/etc/asterisk/rpt.conf
+perl -pi -e 's/^rxchannel\s+=\s+SimpleUSB\/1999/rxchannel = dahdi\/pseudo/g' \
+	/etc/asterisk/rpt.conf
+
 /usr/sbin/asterisk -f
 
